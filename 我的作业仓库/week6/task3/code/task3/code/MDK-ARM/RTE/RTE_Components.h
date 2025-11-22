496dff10788f54df7c5cf59f60dcb5b98ea71c11
A2025111 <13695189351@163.com>
2025-11-22 23:25:49 +0800

    Add files via upload

---
+++ 我的作业仓库/week6/task3/code/task3/code/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'task3' 
+ * Target:  'task3' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
